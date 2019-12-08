45d155aa7fcd6d61a5590f87371da68efb8655f9
Karl <razielim@gmail.com>
2019-12-08 20:34:00 +0100

    - Added a bunch of BarGraph stacking tests - Changed 2 most simple bargraph examples to only use positive numbers. Just so that we have one test with only positive numbers on the main series.
    - Fixed some bugs uncovered by the new tests

--- Tests/SwiftPlotTests/BarChart/barchart-examples.swift
+++ Tests/SwiftPlotTests/BarChart/barchart-examples.swift
@@ -15,7 +15,7 @@ extension BarchartTests {
   /// A simple vertical BarGraph.
   func testBarchart() throws {
     let x:[String] = ["2008","2009","2010","2011"]
-    let y:[Float] = [320,-100,420,500]
+    let y:[Float] = [320,100,420,500]
     let barGraph = y.plots.barChart() { graph in
       graph.color = .orange
       graph.formatter = .array(x)
@@ -48,7 +48,7 @@ extension BarchartTests {
   /// A BarGraph with horizontal orientation.
   func testBarchartOrientationHorizontal() throws {
     let x:[String] = ["2008","2009","2010","2011"]
-    let y:[Float] = [320,-100,420,500]
+    let y:[Float] = [320,100,420,500]
     let barGraph = y.plots.barChart() { graph in
       graph.color = .orange
       graph.graphOrientation = .horizontal
